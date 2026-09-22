f7b958a34363a63a206b4ed9ddb728fa6364652e
Henry Persen <persenhenry@gmail.com>
2026-09-22 14:13:20 +0200

    tirsdag

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-let numarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for(let x in numarr) {
-    if(numarr[x] % 2 == 0){
-        console.log(numarr[x], "er et partall")
+grid = document.getElementById("container")
+
+cellsize = 64
+
+gridheight = 4
+gridwidth = 4
+grid.style = "--columns: " + gridwidth.toString()
+console.log(grid.style)
+grid.style.width = (gridwidth * cellsize).toString() + "px"
+grid.style.height = (gridheight * cellsize).toString() + "px"
+
+
+cells = []
+
+function changecolor(cell){
+    if(cell.style.background != "red"){
+        cell.style.background = "red"
     } else {
-        console.log(numarr[x], "er et oddetall")
+        cell.style.background = "white"
     }
+}
+
+function cellclicked(cell){
+    let index = cells.indexOf(cell)
+    
+    changecolor(cell)
+    if(index % 4 == 0){
+        
+    } else {
+        changecolor(cells[index+1])
+    }
+    changecolor(cells[index-1])
+    
+    changecolor(cells[index-gridwidth])
+    changecolor(cells[index-gridwidth+1])
+    changecolor(cells[index-gridwidth-1])
+
+    changecolor(cells[index+gridwidth])
+    changecolor(cells[index+gridwidth+1])
+    changecolor(cells[index+gridwidth-1])
+}
+
+for(let i = 0; i < gridheight * gridwidth; i++){
+    var gridcell = document.createElement("div");
+    gridcell.style.height =  cellsize.toString + "px";
+    gridcell.style.width = cellsize.toString() + "px";
+    gridcell.style.margin = "0px"
+    gridcell.style.borderStyle = "Solid";
+    gridcell.style.borderWidth = "1px"
+    grid.appendChild(gridcell)
+    cells.push(gridcell)
+}
+for(let i of cells){
+    i.addEventListener("click", () => cellclicked(i))
 }
